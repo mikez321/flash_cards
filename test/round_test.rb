67bab4e5d1a01180e85c9863770d86318ed43e8a
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-30 21:10:02 -0700

    Change first test to look for round instance

--- test/round_test.rb
+++ test/round_test.rb
@@ -6,7 +6,7 @@ require './lib/deck'
 require './lib/round'
 
 class RoundTest < Minitest::Test
-  def test_at_the_beginning_of_a_round_it_has_a_deck
+  def test_it_exists
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -14,7 +14,7 @@ class RoundTest < Minitest::Test
     deck = Deck.new(cards)
     round = Round.new(deck)
 
-    assert_instance_of Deck, deck
+    assert_instance_of Round, round
   end
 
   def test_when_round_starts_no_turns_have_been_taken
