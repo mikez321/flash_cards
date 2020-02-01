441835d204b373fd212564136b471c49b4c2992c
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-31 20:56:03 -0700

    Create method to list all categories in deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,9 @@
 class Deck
-  attr_accessor :cards
+  attr_accessor :cards, :categories
   def initialize(cards= [])
     @cards = cards
+    @categories = []
+    categories_in_deck
   end
 
   def count
@@ -16,5 +18,13 @@ class Deck
     cards.select do |card|
         card.category == category
     end
+  end
+
+  def categories_in_deck
+    starting_categories = []
+    cards.each do |card|
+      starting_categories << card.category
+    end
+    @categories = starting_categories.uniq
   end
 end
