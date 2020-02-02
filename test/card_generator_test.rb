fc4b5b09ebf0eff5e926c7813017507146cb5fd1
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-02 08:32:22 -0700

    Remove unused requires

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -1,11 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
-require './lib/turn'
-require './lib/deck'
-require './lib/round'
 require './lib/card_generator'
-# require './lib/cards.txt'
 
 
 
