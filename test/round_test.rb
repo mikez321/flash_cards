a4e4ed5c68e713fe1812e69b1f43950ed2172cbc
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-02 09:17:05 -0700

    Insignifigant spacing changes

--- test/round_test.rb
+++ test/round_test.rb
@@ -51,7 +51,6 @@ class RoundTest < Minitest::Test
   end
 
   def test_a_turn_exists_when_a_new_turn_is_taken_in_the_round
-
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -65,7 +64,6 @@ class RoundTest < Minitest::Test
   end
 
   def test_turns_give_feedback
-
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
