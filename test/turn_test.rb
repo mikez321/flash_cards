a729601c3b837367fd3e717acdacec7d8a5eb1b7
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-02 08:30:14 -0700

    Make adjustments in tests to more accurately reflect pry workflow

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -5,39 +5,30 @@ require './lib/card'
 
 class TurnTest < Minitest::Test
 
-  def test_turns_access_cards_and_contain_guesses
+  def test_a_turn_has_a_card
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Bears", card)
-
-    assert_equal "What is the capital of Alaska?", card.question
-    assert_equal "Juneau", card.answer
-    assert_equal "Bears", turn.guess
-  end
-
-  def test_can_see_a_turns_guess
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Bears", card)
-
-    assert_equal "Bears", turn.guess
-  end
-
-  def test_card_method_returns_card_info
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
 
     assert_equal card, turn.card
   end
 
-  def test_card_and_guess_get_compared_for_correctness
+  def test_a_turn_has_a_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Bears", card)
+
+    assert_equal "Bears", turn.guess
+  end
+
+  def test_turns_can_be_correct_or_not_correct
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn1 = Turn.new("Bears", card)
     turn2 = Turn.new("Juneau", card)
 
-    refute turn1.correct?
-    assert turn2.correct?
+    assert_equal false, turn1.correct?
+    assert_equal true,  turn2.correct?
   end
 
-  def test_give_proper_feedback_answer
+  def test_give_feedback_based_on_correctness
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn1 = Turn.new("Bears", card)
     turn2 = Turn.new("Juneau", card)
